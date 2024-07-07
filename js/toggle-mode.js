b1861a622d05755e17c31dfccf79b9f1dc75d027
risu <cleber.risu@outlook.com>
2024-07-07 20:35:43 -0300

    init

---
+++ js/toggle-mode.js
@@ -0,0 +1,15 @@
+let lightMode = true;
+
+const buttonToggle = document.querySelector("#toggle-mode");
+
+buttonToggle.addEventListener("click", (even) => {
+  document.body.classList.toggle("dark");
+
+  const mode = lightMode ? "light" : "dark";
+
+  even.currentTarget.querySelector(
+    "span"
+  ).textContent = `${mode} mode ativado.`;
+
+  lightMode = !lightMode;
+});
